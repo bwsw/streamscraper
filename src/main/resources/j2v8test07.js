96977a94fcf230b1497d1cb3d273e0599c1d6f99
Bitworks, Ltd <bitworks@bw-sw.com>
2015-12-22 17:38:51 +0600

    Code reorganized. Created package com.bwsw.streamscraper.models.adapters.j2v8.

---
+++ src/main/resources/j2v8test07.js
@@ -0,0 +1,15 @@
+var handler = {
+    'init': function () {
+    },
+    'shutdown': function() {
+        // on shutdown
+    },
+    'commit': function() {
+        // on commit
+    },
+    'process': function() {
+        // on process
+	var i = get_event();
+	//log(i.key);
+    }
+};
